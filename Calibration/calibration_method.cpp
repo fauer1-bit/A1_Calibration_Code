d6e16c2d51ccec33ee37df881f303d66e8121285
fauer1-bit <auerfrederick@gmail.com>
2025-03-01 14:41:18 +0100

    Up until extracting parameters.

--- Calibration/calibration_method.cpp
+++ Calibration/calibration_method.cpp
@@ -70,119 +70,119 @@ bool Calibration::calibration(
     // length. With 'std::vector', you can append/delete/insert elements, and much more. The 'std::vector' can store
     // not only 'double', but also any other types of objects. In case you may want to learn more about 'std::vector'
     // check here: https://en.cppreference.com/w/cpp/container/vector
-    std::vector<double> array = {1, 3, 3, 4, 7, 6, 2, 8, 2, 8, 3, 2, 4, 9, 1, 7, 3, 23, 2, 3, 5, 2, 1, 5, 8, 9, 22};
-    array.push_back(5); // append 5 to the array (so the size will increase by 1).
-    array.insert(array.end(), 10, 3);  // append ten 3 (so the size will grow by 10).
-
-    /// To access the value of an element.
-    double a = array[2];
-
-    /// define a 2D vector/point
-    Vector2D b(1.1, 2.2);
-
-    /// define a 3D vector/point
-    Vector3D c(1.1, 2.2, 3.3);
-
-    /// get the Cartesian coordinates of a (a is treated as Homogeneous coordinates)
-    Vector2D p = c.cartesian();
-
-    /// get the Homogeneous coordinates of p
-    Vector3D q = p.homogeneous();
-
-    /// the length of a vector
-    double len = p.length();
-    /// the squared length of a vector
-    double sqr_len = p.length2();
-
-    /// the dot product of two vectors
-    double dot_prod = dot(p, q);
-
-    /// the cross product of two vectors
-    Vector cross_prod = cross(c, q);
-
-    /// normalize this vector
-    cross_prod.normalize();
-
-    // Define an m-by-n double valued matrix.
-    // Here I use the above array to initialize it. You can also use A(i, j) to initialize/modify/access its elements.
-    const int m = 6, n = 5;
-    Matrix A(m, n, array.data());    // 'array.data()' returns a pointer to the array.
-    std::cout << "M: \n" << A << std::endl;
-
-    /// define a 3 by 4 matrix (and all elements initialized to 0.0)
-    Matrix M(3, 4, 0.0);
-
-    /// set first row by a vector
-    M.set_row(0, Vector4D(1.1, 2.2, 3.3, 4.4));
-
-    /// set second column by a vector
-    M.set_column(1, Vector3D(5.5, 5.5, 5.5));
-
-    /// define a 3 by 3 matrix (and all elements initialized to 0.0)
-    Matrix33 B;
-
-    /// define and initialize a 3 by 3 matrix
-    Matrix33 T(1.1, 2.2, 3.3,
-               0, 2.2, 3.3,
-               0, 0, 1);
-
-    /// define and initialize a 3 by 4 matrix
-    Matrix34 P(1.1, 2.2, 3.3, 0,
-               0, 2.2, 3.3, 1,
-               0, 0, 1, 1);
-
-    /// define a 15 by 9 matrix (and all elements initialized to 0.0)
-    Matrix W(15, 9, 0.0);
-    /// set the first row by a 9-dimensional vector
-    W.set_row(0, {0, 1, 2, 3, 4, 5, 6, 7, 8}); // {....} is equivalent to a std::vector<double>
-
-    /// get the number of rows.
-    int num_rows = W.rows();
-
-    /// get the number of columns.
-    int num_cols = W.cols();
-
-    /// get the the element at row 1 and column 2
-    double value = W(1, 2);
-
-    /// get the last column of a matrix
-    Vector last_column = W.get_column(W.cols() - 1);
-
-    /// define a 3 by 3 identity matrix
-    Matrix33 I = Matrix::identity(3, 3, 1.0);
-
-    /// matrix-vector product
-    Vector3D v = M * Vector4D(1, 2, 3, 4); // M is 3 by 4
-
-    Matrix U(m, m, 0.0);   // initialized with 0s
-    Matrix S(m, n, 0.0);   // initialized with 0s
-    Matrix V(n, n, 0.0);   // initialized with 0s
-
-    // Compute the SVD decomposition of A
-    svd_decompose(A, U, S, V);
-
-    // Now let's check if the SVD result is correct
-
-    // Check 1: U is orthogonal, so U * U^T must be identity
-    std::cout << "U*U^T: \n" << U * transpose(U) << std::endl;
-
-    // Check 2: V is orthogonal, so V * V^T must be identity
-    std::cout << "V*V^T: \n" << V * transpose(V) << std::endl;
-
-    // Check 3: S must be a diagonal matrix
-    std::cout << "S: \n" << S << std::endl;
-
-    // Check 4: according to the definition, A = U * S * V^T
-    std::cout << "M - U * S * V^T: \n" << A - U * S * transpose(V) << std::endl;
-
-    // Compute the inverse of a matrix
-    Matrix invT;
-    inverse(T, invT);
-    // Let's check if the inverse is correct
-    std::cout << "T * invT: \n" << T * invT << std::endl;
-
-    // TODO: the above code just demonstrates some useful data structures and APIs. Please remove all above code in your
-    //       final submission.
+    // std::vector<double> array = {1, 3, 3, 4, 7, 6, 2, 8, 2, 8, 3, 2, 4, 9, 1, 7, 3, 23, 2, 3, 5, 2, 1, 5, 8, 9, 22};
+    // array.push_back(5); // append 5 to the array (so the size will increase by 1).
+    // array.insert(array.end(), 10, 3);  // append ten 3 (so the size will grow by 10).
+    //
+    // /// To access the value of an element.
+    // double a = array[2];
+    //
+    // /// define a 2D vector/point
+    // Vector2D b(1.1, 2.2);
+    //
+    // /// define a 3D vector/point
+    // Vector3D c(1.1, 2.2, 3.3);
+    //
+    // /// get the Cartesian coordinates of a (a is treated as Homogeneous coordinates)
+    // Vector2D p = c.cartesian();
+    //
+    // /// get the Homogeneous coordinates of p
+    // Vector3D q = p.homogeneous();
+    //
+    // /// the length of a vector
+    // double len = p.length();
+    // /// the squared length of a vector
+    // double sqr_len = p.length2();
+    //
+    // /// the dot product of two vectors
+    // double dot_prod = dot(p, q);
+    //
+    // /// the cross product of two vectors
+    // Vector cross_prod = cross(c, q);
+    //
+    // /// normalize this vector
+    // cross_prod.normalize();
+    //
+    // // Define an m-by-n double valued matrix.
+    // // Here I use the above array to initialize it. You can also use A(i, j) to initialize/modify/access its elements.
+    // const int m = 6, n = 5;
+    // Matrix A(m, n, array.data());    // 'array.data()' returns a pointer to the array.
+    // std::cout << "M: \n" << A << std::endl;
+    //
+    // /// define a 3 by 4 matrix (and all elements initialized to 0.0)
+    // Matrix M(3, 4, 0.0);
+    //
+    // /// set first row by a vector
+    // M.set_row(0, Vector4D(1.1, 2.2, 3.3, 4.4));
+    //
+    // /// set second column by a vector
+    // M.set_column(1, Vector3D(5.5, 5.5, 5.5));
+    //
+    // /// define a 3 by 3 matrix (and all elements initialized to 0.0)
+    // Matrix33 B;
+    //
+    // /// define and initialize a 3 by 3 matrix
+    // Matrix33 T(1.1, 2.2, 3.3,
+    //            0, 2.2, 3.3,
+    //            0, 0, 1);
+    //
+    // /// define and initialize a 3 by 4 matrix
+    // Matrix34 P(1.1, 2.2, 3.3, 0,
+    //            0, 2.2, 3.3, 1,
+    //            0, 0, 1, 1);
+    //
+    // /// define a 15 by 9 matrix (and all elements initialized to 0.0)
+    // Matrix W(15, 9, 0.0);
+    // /// set the first row by a 9-dimensional vector
+    // W.set_row(0, {0, 1, 2, 3, 4, 5, 6, 7, 8}); // {....} is equivalent to a std::vector<double>
+    //
+    // /// get the number of rows.
+    // int num_rows = W.rows();
+    //
+    // /// get the number of columns.
+    // int num_cols = W.cols();
+    //
+    // /// get the the element at row 1 and column 2
+    // double value = W(1, 2);
+    //
+    // /// get the last column of a matrix
+    // Vector last_column = W.get_column(W.cols() - 1);
+    //
+    // /// define a 3 by 3 identity matrix
+    // Matrix33 I = Matrix::identity(3, 3, 1.0);
+    //
+    // /// matrix-vector product
+    // Vector3D v = M * Vector4D(1, 2, 3, 4); // M is 3 by 4
+    //
+    // Matrix U(m, m, 0.0);   // initialized with 0s
+    // Matrix S(m, n, 0.0);   // initialized with 0s
+    // Matrix V(n, n, 0.0);   // initialized with 0s
+    //
+    // // Compute the SVD decomposition of A
+    // svd_decompose(A, U, S, V);
+    //
+    // // Now let's check if the SVD result is correct
+    //
+    // // Check 1: U is orthogonal, so U * U^T must be identity
+    // std::cout << "U*U^T: \n" << U * transpose(U) << std::endl;
+    //
+    // // Check 2: V is orthogonal, so V * V^T must be identity
+    // std::cout << "V*V^T: \n" << V * transpose(V) << std::endl;
+    //
+    // // Check 3: S must be a diagonal matrix
+    // std::cout << "S: \n" << S << std::endl;
+    //
+    // // Check 4: according to the definition, A = U * S * V^T
+    // std::cout << "M - U * S * V^T: \n" << A - U * S * transpose(V) << std::endl;
+    //
+    // // Compute the inverse of a matrix
+    // Matrix invT;
+    // inverse(T, invT);
+    // // Let's check if the inverse is correct
+    // std::cout << "T * invT: \n" << T * invT << std::endl;
+    //
+    // // TODO: the above code just demonstrates some useful data structures and APIs. Please remove all above code in your
+    // //       final submission.
 
     //--------------------------------------------------------------------------------------------------------------
     // implementation starts ...
@@ -236,12 +236,50 @@ bool Calibration::calibration(
 
     }
 
+    // Check matrix
+    std::cout << "Matrix P:" << std::endl;
+    for (int i = 0; i < mp_length; ++i) {
+        std::cout << "\tRow " << i << ": " << P.get_row(i) << std::endl;
+    }
+
 
     // TODO: solve for M (the whole projection matrix, i.e., M = K * [R, t]) using SVD decomposition.
     //   Optional: you can check if your M is correct by applying M on the 3D points. If correct, the projected point
     //             should be very close to your input images points.
+    // let P = UDV^T, set m equal to the lac column of V. (where
+
+    // Matrix U, D and V initialise for SVD on matrix P
+    Matrix U(mp_length, mp_length);
+    Matrix D(mp_length, 12); // diagonal matrix
+    Matrix V(12, 12);
+
+    svd_decompose(P, U, D, V);
+
+    //get last vector for m
+    Vector m = V.get_column(V.cols() -1);
+
+    //Construct Matrix M from m
+    Matrix34 M(
+        m[0], m[1], m[2], m[3],
+        m[4], m[5], m[6], m[7],
+        m[8], m[9], m[10], m[11]
+    );
+
+    //check matrix
+    std::cout << "Projection Matrix M: \n" << M << std::endl;
+
+    // extract the R (rotation) and translation vector t from M
+
+    Vector3D a1(M[0][0], M[0][1], M[0][2]);
+    Vector3D a2(M[1][0], M[1][1], M[1][2]);
+    Vector3D a3(M[2][0], M[2][1], M[2][2]);
+
+    Vector3D b(M[0][3], M[1][3], M[2][3]);
+
 
     // TODO: extract intrinsic parameters from M.
+
+
 
     // TODO: extract extrinsic parameters from M.
 
